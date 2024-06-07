3a8f43c68000bfc367521c5fbd313b3c4a98b988
Harsh Singh <harshsingh16d@gmail.com>
2024-06-07 11:11:37 +0530

    published zod verfier package for verifying types in users and blogs

---
+++ packages/dist/index.d.ts
@@ -0,0 +1,54 @@
+import z from "zod";
+export declare const signupInput: z.ZodObject<{
+    email: z.ZodString;
+    password: z.ZodString;
+    name: z.ZodOptional<z.ZodString>;
+}, "strip", z.ZodTypeAny, {
+    email: string;
+    password: string;
+    name?: string | undefined;
+}, {
+    email: string;
+    password: string;
+    name?: string | undefined;
+}>;
+export type SignupType = z.infer<typeof signupInput>;
+export declare const signinInput: z.ZodObject<{
+    email: z.ZodString;
+    password: z.ZodString;
+}, "strip", z.ZodTypeAny, {
+    email: string;
+    password: string;
+}, {
+    email: string;
+    password: string;
+}>;
+export type SigninType = z.infer<typeof signinInput>;
+export declare const createPostInput: z.ZodObject<{
+    title: z.ZodString;
+    content: z.ZodString;
+    imageUrl: z.ZodOptional<z.ZodString>;
+}, "strip", z.ZodTypeAny, {
+    title: string;
+    content: string;
+    imageUrl?: string | undefined;
+}, {
+    title: string;
+    content: string;
+    imageUrl?: string | undefined;
+}>;
+export type CreatePostType = z.infer<typeof createPostInput>;
+export declare const updatePostInput: z.ZodObject<{
+    title: z.ZodOptional<z.ZodString>;
+    content: z.ZodOptional<z.ZodString>;
+    imageUrl: z.ZodOptional<z.ZodString>;
+}, "strip", z.ZodTypeAny, {
+    title?: string | undefined;
+    content?: string | undefined;
+    imageUrl?: string | undefined;
+}, {
+    title?: string | undefined;
+    content?: string | undefined;
+    imageUrl?: string | undefined;
+}>;
+export type UpdatePostType = z.infer<typeof updatePostInput>;
